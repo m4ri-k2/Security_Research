0d1c70dbca641afebdc92e69eac9eae029bb789b
m4ri <0xk2mari@gmail.com>
2026-01-07 11:58:02 +0900

    Refactor DES functions for improved block handling
    Updated DES implementation to include additional parameters in functions for better handling of blocks and keys. Refactored function signatures and added necessary logic for encryption and key scheduling.

--- project-des-from-scratch/DES.c
+++ project-des-from-scratch/DES.c
@@ -9,7 +9,7 @@
 
 int password(char pass0[6], char pass1[6]);
 void compared(int ret);
-void plain(char message0[301], int message1[2560], int blocks[40][64]);
+void plain(char message0[301], int message1[2560], int blocks[40][64], int* nblocks);
 void IP(const int input[40][64], int output[40][64], const int ip[64]);
 void divide(int output[40][64], int L[40][32], int R[40][32]);
 void ebox(int R[40][32], int EBOX[40][48], const int Etable[48]);
@@ -21,87 +21,17 @@ void shift(int C[28], int D[28], int round);
 void CD1(int C[28], int D[28], int CD[56]);
 void PC2(const int pc2[48], int out48[48], int CD[56]);
 const int shift_table[16] = { 1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1 };
-void F(const int R[32], const int roundkey[48], int out32[32], const int Etable[48], const int Ptable[32], const int SBOX[8][4][16]);
+void F(const int R[32], const int roundkey1[48], int out32[32], const int Etable[48], const int Ptable[32], const int SBOX[8][4][16]);
 static void xor48(const int a[48], const int b[48], int out[48]);
 static void pbox32(const int in32[32], int out32[32], const int Ptable[32]);
 static void sbox_6_to_4(const int six[6], const int S[4][16], int out4[4]);
 static void sboxes_48_to_32(const int in48[48], int out32[32], const int SBOX[8][4][16]);
 static void xor32(const int AA[32], const int BB[32], int outout[32]);
-void one_round(const int L[32], const int R[32], const int roundkey[48], int newL[32], int newR[32], const int Etable[48], const int Ptable[32], const int SBOX[8][4][16]);
-//---
+void one_round(const int L[32], const int R[32], const int roundkey1[48], int newL[32], int newR[32], const int Etable[48], const int Ptable[32], const int SBOX[8][4][16]);
+unsigned long long des_encrypt_block_bits(const int in64[64], const int ip[64], const int fp[64], const int Etable[48], const int Ptable[32], const int SBOX[8][4][16], const int roundkey2[16][48]);
 
-const int Ptable[32] =
-{
-    16,7,20,21,29,12,28,17,
-    1,15,23,26,5,18,31,10,
-    2,8,24,14,32,27,3,9,
-    19,13,30,6,22,11,4,25
-};
 
-const int SBOX[8][4][16] =
-{
-    { // S1
-        {14,4,13,1,2,15,11,8,3,10,6,12,5,9,0,7},
-        {0,15,7,4,14,2,13,1,10,6,12,11,9,5,3,8},
-        {4,1,14,8,13,6,2,11,15,12,9,7,3,10,5,0},
-        {15,12,8,2,4,9,1,7,5,11,3,14,10,0,6,13}
-    },
-    { // S2
-        {15,1,8,14,6,11,3,4,9,7,2,13,12,0,5,10},
-        {3,13,4,7,15,2,8,14,12,0,1,10,6,9,11,5},
-        {0,14,7,11,10,4,13,1,5,8,12,6,9,3,2,15},
-        {13,8,10,1,3,15,4,2,11,6,7,12,0,5,14,9}
-    },
-    { // S3
-        {10,0,9,14,6,3,15,5,1,13,12,7,11,4,2,8},
-        {13,7,0,9,3,4,6,10,2,8,5,14,12,11,15,1},
-        {13,6,4,9,8,15,3,0,11,1,2,12,5,10,14,7},
-        {1,10,13,0,6,9,8,7,4,15,14,3,11,5,2,12}
-    },
-    { // S4
-        {7,13,14,3,0,6,9,10,1,2,8,5,11,12,4,15},
-        {13,8,11,5,6,15,0,3,4,7,2,12,1,10,14,9},
-        {10,6,9,0,12,11,7,13,15,1,3,14,5,2,8,4},
-        {3,15,0,6,10,1,13,8,9,4,5,11,12,7,2,14}
-    },
-    { // S5
-        {2,12,4,1,7,10,11,6,8,5,3,15,13,0,14,9},
-        {14,11,2,12,4,7,13,1,5,0,15,10,3,9,8,6},
-        {4,2,1,11,10,13,7,8,15,9,12,5,6,3,0,14},
-        {11,8,12,7,1,14,2,13,6,15,0,9,10,4,5,3}
-    },
-    { // S6
-        {12,1,10,15,9,2,6,8,0,13,3,4,14,7,5,11},
-        {10,15,4,2,7,12,9,5,6,1,13,14,0,11,3,8},
-        {9,14,15,5,2,8,12,3,7,0,4,10,1,13,11,6},
-        {4,3,2,12,9,5,15,10,11,14,1,7,6,0,8,13}
-    },
-    { // S7
-        {4,11,2,14,15,0,8,13,3,12,9,7,5,10,6,1},
-        {13,0,11,7,4,9,1,10,14,3,5,12,2,15,8,6},
-        {1,4,11,13,12,3,7,14,10,15,6,8,0,5,9,2},
-        {6,11,13,8,1,4,10,7,9,5,0,15,14,2,3,12}
-    },
-    { // S8
-        {13,2,8,4,6,15,11,1,10,9,3,14,5,0,12,7},
-        {1,15,13,8,10,3,7,4,12,5,6,11,0,14,9,2},
-        {7,11,4,1,9,12,14,2,0,6,10,13,15,3,5,8},
-        {2,1,14,7,4,10,8,13,15,12,9,0,3,5,6,11}
-    }
-};
-
-const int fp[64] = {
-    40,8,48,16,56,24,64,32,
-    39,7,47,15,55,23,63,31,
-    38,6,46,14,54,22,62,30,
-    37,5,45,13,53,21,61,29,
-    36,4,44,12,52,20,60,28,
-    35,3,43,11,51,19,59,27,
-    34,2,42,10,50,18,58,26,
-    33,1,41,9,49,17,57,25
-};
-
-int main()
+int main(void)
 {
     SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 5);
     printf("HELLO! I am m4ri, and I am the developer of this program :)");
@@ -112,16 +42,21 @@ int main()
     printf("\n\n=========================================================================================");
     printf("\n\n >> Please enter a 5-character password : ");
 
-    char pass0[6];
-    char pass1[6];
+    // --- password
+    char pass0[6] = { 0 };
+    char pass1[6] = { 0 };
     int ret = password(pass0, pass1);
     compared(ret);
-    char message0[301];
+
+    // --- plaintext -> bits -> blocks[40][64]
+    char message0[301] = { 0 };
     int message1[2560] = { 0 };
     int blocks[40][64] = { 0 };
-    plain(message0, message1, blocks);
+    int nblocks = 0;
+    plain(message0, message1, blocks, &nblocks);
 
-    // --- IP
+    // ===================== DES TABLES =====================
+
     const int ip[64] =
     {
         58,50,42,34,26,18,10,2,
@@ -134,13 +69,91 @@ int main()
         63,55,47,39,31,23,15,7
     };
 
-    int output[40][64] = { 0 };
-    int L[40][32] = { 0 };
-    int R[40][32] = { 0 };
+    const int fp[64] =
+    {
+        40,8,48,16,56,24,64,32,
+        39,7,47,15,55,23,63,31,
+        38,6,46,14,54,22,62,30,
+        37,5,45,13,53,21,61,29,
+        36,4,44,12,52,20,60,28,
+        35,3,43,11,51,19,59,27,
+        34,2,42,10,50,18,58,26,
+        33,1,41,9,49,17,57,25
+    };
 
-    IP(blocks, output, ip);
-    divide(output, L, R);
+    const int Etable[48] =
+    {
+        32,1,2,3,4,5,
+        4,5,6,7,8,9,
+        8,9,10,11,12,13,
+        12,13,14,15,16,17,
+        16,17,18,19,20,21,
+        20,21,22,23,24,25,
+        24,25,26,27,28,29,
+        28,29,30,31,32,1
+    };
 
+    const int Ptable[32] =
+    {
+        16,7,20,21,29,12,28,17,
+        1,15,23,26,5,18,31,10,
+        2,8,24,14,32,27,3,9,
+        19,13,30,6,22,11,4,25
+    };
+
+    const int SBOX[8][4][16] =
+    {
+        { // S1
+            {14,4,13,1,2,15,11,8,3,10,6,12,5,9,0,7},
+            {0,15,7,4,14,2,13,1,10,6,12,11,9,5,3,8},
+            {4,1,14,8,13,6,2,11,15,12,9,7,3,10,5,0},
+            {15,12,8,2,4,9,1,7,5,11,3,14,10,0,6,13}
+        },
+        { // S2
+            {15,1,8,14,6,11,3,4,9,7,2,13,12,0,5,10},
+            {3,13,4,7,15,2,8,14,12,0,1,10,6,9,11,5},
+            {0,14,7,11,10,4,13,1,5,8,12,6,9,3,2,15},
+            {13,8,10,1,3,15,4,2,11,6,7,12,0,5,14,9}
+        },
+        { // S3
+            {10,0,9,14,6,3,15,5,1,13,12,7,11,4,2,8},
+            {13,7,0,9,3,4,6,10,2,8,5,14,12,11,15,1},
+            {13,6,4,9,8,15,3,0,11,1,2,12,5,10,14,7},
+            {1,10,13,0,6,9,8,7,4,15,14,3,11,5,2,12}
+        },
+        { // S4
+            {7,13,14,3,0,6,9,10,1,2,8,5,11,12,4,15},
+            {13,8,11,5,6,15,0,3,4,7,2,12,1,10,14,9},
+            {10,6,9,0,12,11,7,13,15,1,3,14,5,2,8,4},
+            {3,15,0,6,10,1,13,8,9,4,5,11,12,7,2,14}
+        },
+        { // S5
+            {2,12,4,1,7,10,11,6,8,5,3,15,13,0,14,9},
+            {14,11,2,12,4,7,13,1,5,0,15,10,3,9,8,6},
+            {4,2,1,11,10,13,7,8,15,9,12,5,6,3,0,14},
+            {11,8,12,7,1,14,2,13,6,15,0,9,10,4,5,3}
+        },
+        { // S6
+            {12,1,10,15,9,2,6,8,0,13,3,4,14,7,5,11},
+            {10,15,4,2,7,12,9,5,6,1,13,14,0,11,3,8},
+            {9,14,15,5,2,8,12,3,7,0,4,10,1,13,11,6},
+            {4,3,2,12,9,5,15,10,11,14,1,7,6,0,8,13}
+        },
+        { // S7
+            {4,11,2,14,15,0,8,13,3,12,9,7,5,10,6,1},
+            {13,0,11,7,4,9,1,10,14,3,5,12,2,15,8,6},
+            {1,4,11,13,12,3,7,14,10,15,6,8,0,5,9,2},
+            {6,11,13,8,1,4,10,7,9,5,0,15,14,2,3,12}
+        },
+        { // S8
+            {13,2,8,4,6,15,11,1,10,9,3,14,5,0,12,7},
+            {1,15,13,8,10,3,7,4,12,5,6,11,0,14,9,2},
+            {7,11,4,1,9,12,14,2,0,6,10,13,15,3,5,8},
+            {2,1,14,7,4,10,8,13,15,12,9,0,3,5,6,11}
+        }
+    };
+
+    // --- key schedule tables
     const int pc1[56] =
     {
         57,49,41,33,25,17,9,
@@ -164,67 +177,44 @@ int main()
         44,49,39,56,34,53,
         46,42,50,36,29,32
     };
-	int key64[64] = { 0 };
-    unsigned char key[8] = { 0 };
+
+    // ===================== KEY SCHEDULE =====================
+    unsigned char key[8] = { 0x13,0x34,0x57,0x79, 0x9B,0xBC,0xDF,0xF1 }; // 테스트키
+    int key64[64] = { 0 };
     int pc1table[56] = { 0 };
-	int C[28] = { 0 };
-	int D[28] = { 0 };
+    int C[28] = { 0 };
+    int D[28] = { 0 };
+    int CD[56] = { 0 };
+    int roundkey2[16][48] = { 0 };
 
     makekey64(key, key64);
     PC1(pc1, pc1table, key64);
     devideCD(C, D, pc1table);
 
-    int Lcur[32] = { 0 };
-    int Rcur[32] = { 0 };
-    int newL[32] = { 0 };
-    int newR[32] = { 0 };
-    int roundKey[16][48] = { 0 };
-    const int Etable[48] =
-    {
-        32,1,2,3,4,5,
-        4,5,6,7,8,9,
-        8,9,10,11,12,13,
-        12,13,14,15,16,17,
-        16,17,18,19,20,21,
-        20,21,22,23,24,25,
-        24,25,26,27,28,29,
-        28,29,30,31,32,1
-    };
-
-    // 초기 상태: L0/R0
-    for (int j = 0; j < 32; j++)
-    {
-        Lcur[j] = L[0][j];
-        Rcur[j] = R[0][j];
-    }
-
-    // 16 rounds
     for (int r = 0; r < 16; r++)
     {
-        one_round(Lcur, Rcur, roundKey[r], newL, newR, Etable, Ptable, SBOX);
-
-        for (int j = 0; j < 32; j++)
-        {
-            Lcur[j] = newL[j];
-            Rcur[j] = newR[j];
-        }
+        shift(C, D, r);
+        CD1(C, D, CD);
+        PC2(pc2, roundkey2[r], CD);
     }
 
-    int preout[64] = { 0 };
+    // ===================== ENCRYPT ALL BLOCKS =====================
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
+    printf("\n >> This is your encrypted message \n : ");
 
-    // swap: R16 || L16
-    for (int i = 0; i < 32; i++)
+    for (int b = 0; b < nblocks; b++)
     {
-        preout[i] = Rcur[i];
-        preout[i + 32] = Lcur[i];
-    }
+        int in64[64] = { 0 };
+        for (int i = 0; i < 64; i++)
+            in64[i] = blocks[b][i];
 
-    int cipher[64] = { 0 };
+        unsigned long long ct =
+            des_encrypt_block_bits(in64, ip, fp, Etable, Ptable, SBOX, roundkey2);
 
-    for (int i = 0; i < 64; i++)
-    {
-        cipher[i] = preout[fp[i] - 1];
+        printf("%016llX", ct);
     }
+    printf("\n");
+
     return 0;
 }
 
@@ -268,7 +258,7 @@ void compared(int ret)
     }
 }
 
-void plain(char message0[301], int message1[2560], int blocks[40][64])
+void plain(char message0[301], int message1[2560], int blocks[40][64], int* nblocks)
 {
     SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 6);
     printf("\n >> Now, Enter your message in 300 characters!");
@@ -278,6 +268,7 @@ void plain(char message0[301], int message1[2560], int blocks[40][64])
     int len = 0; // 입력글자 수
     int pad = 0; // 1블록=1패드
     int block = 8; // 1블록 크기 지정
+	int totalLen = 0; // 전체 입력글자 수
 
     fgets(message0, 301, stdin); // message0에 300자 받기
 
@@ -310,7 +301,9 @@ void plain(char message0[301], int message1[2560], int blocks[40][64])
         message0[len + i] = (char)pad; // 입력버퍼에서 남은 자리에 패딩 채워서 1블록(1pad) 완성시키는 거
     }
     message0[len + pad] = '\0'; // 엔터키 칠 때까지 받기
-    int totalLen = len + pad; // 입력글자 수 + 패드 수 = 전체 입력글자
+    totalLen = len + pad; // 입력글자 수 + 패드 수 = 전체 입력글자
+    *nblocks = totalLen / 8;
+
     int idx = 0;
 
     for (int j = 0; j < totalLen; j++) // 블록(암호화 단위)나누는 코드. 
@@ -438,7 +431,6 @@ void PC2(const int pc2[48], int out48[48], int CD[56])
     for (int i = 0; i < 48; i++)
     {
         out48[i] = CD[pc2[i] - 1];
-
     }
 }
 
@@ -491,7 +483,7 @@ static void sboxes_48_to_32(const int in48[48], int out32[32], const int SBOX[8]
     }
 }
 
-void F(const int R[32], const int roundkey[48], int out32[32], const int Etable[48], const int Ptable[32], const int SBOX[8][4][16])
+void F(const int R[32], const int roundkey1[48], int out32[32], const int Etable[48], const int Ptable[32], const int SBOX[8][4][16])
 {
     int ER[48];     // E(R)
     int X[48];      // ER XOR roundKey
@@ -504,7 +496,7 @@ void F(const int R[32], const int roundkey[48], int out32[32], const int Etable[
         ER[j] = R[Etable[j] - 1];
     }
     // 2) X = ER XOR roundKey
-    xor48(ER, roundkey, X);
+    xor48(ER, roundkey1, X);
     // 3) S_out = S-boxes(X)
     sboxes_48_to_32(X, S_out, SBOX);
     // 4) P_out = P(S_out)
@@ -525,13 +517,54 @@ static void xor32(const int AA[32], const int BB[32], int outout[32])
 }
 
 
-void one_round(const int L[32], const int R[32], const int roundKey[48], int newL[32], int newR[32], const int Etable[48], const int Ptable[32], const int SBOX[8][4][16])
+void one_round(const int L[32], const int R[32], const int roundkey2[48], int newL[32], int newR[32], const int Etable[48], const int Ptable[32], const int SBOX[8][4][16])
 {
     int f_out[32];
-    F(R, roundKey, f_out, Etable, Ptable, SBOX);
+    F(R, roundkey2, f_out, Etable, Ptable, SBOX);
     for (int i = 0; i < 32; i++)
     {
         newL[i] = R[i];
     }
     xor32(L, f_out, newR);
 }
+
+unsigned long long des_encrypt_block_bits(const int in64[64], const int ip[64], const int fp[64], const int Etable[48], const int Ptable[32], const int SBOX[8][4][16], const int roundkey2[16][48])
+{
+    int perm[64] = { 0 };
+    for (int i = 0; i < 64; i++)
+        perm[i] = in64[ip[i] - 1];
+
+    // 2) divide
+    int Lcur[32] = { 0 }, Rcur[32] = { 0 }, newL[32] = { 0 }, newR[32] = { 0 };
+    for (int i = 0; i < 32; i++) {
+        Lcur[i] = perm[i];
+        Rcur[i] = perm[i + 32];
+    }
+
+    // 3) 16 rounds
+    for (int r = 0; r < 16; r++) {
+        one_round(Lcur, Rcur, roundkey2[r], newL, newR, Etable, Ptable, SBOX);
+        for (int i = 0; i < 32; i++) {
+            Lcur[i] = newL[i];
+            Rcur[i] = newR[i];
+        }
+    }
+
+    // 4) swap
+    int preout[64] = { 0 };
+    for (int i = 0; i < 32; i++) {
+        preout[i] = Rcur[i];
+        preout[i + 32] = Lcur[i];
+    }
+
+    // 5) FP + pack to u64
+    unsigned long long out = 0;
+    for (int i = 0; i < 64; i++) {
+        int bit = preout[fp[i] - 1] & 1;
+        out = (out << 1) | (unsigned long long)bit;
+    }
+    
+	return out;
+  
+}
+
